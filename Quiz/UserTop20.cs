b59b2962e7fdea9bf74426ec1d083857d470d461
Konstantin <turchakkonstantin@gmail.com>
2023-02-08 16:09:06 +0200

    1

---
+++ Quiz/UserTop20.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz
+{
+    internal class UserTop20
+    {
+        public string name;
+        public int value;
+        public UserTop20(string name, int value)
+        {
+            this.name = name;
+            this.value = value;
+        }
+    }
+}
